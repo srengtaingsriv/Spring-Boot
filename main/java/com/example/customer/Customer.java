d653914e769b8a8bfcbd33b1b6673952dba400f4
srengtaingsriv <156218999+srengtaingsriv@users.noreply.github.com>
2024-07-18 14:37:20 +0700

    Customer.java

--- main/java/com/example/customer/Customer.java
+++ main/java/com/example/customer/Customer.java
@@ -1,58 +1,16 @@
-package com.example.customer;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Customer {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private String fName;
-    private String lName;
-    private  String phone;
-    public Customer() {
+    private Long id;
+    private String name;
+    private String email;
 
-    }
-
-    public Customer(int id, String fName, String lName, String phone) {
-        this.id = id;
-        this.fName = fName;
-        this.lName = lName;
-        this.phone = phone;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getfName() {
-        return fName;
-    }
-
-    public void setfName(String fName) {
-        this.fName = fName;
-    }
-
-    public String getlName() {
-        return lName;
-    }
-
-    public void setlName(String lName) {
-        this.lName = lName;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
+    // Getters and Setters
 }
